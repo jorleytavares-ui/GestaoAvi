fe4090dc6e227db4c8f4e300b2a03b1b4d1d5216
jorleytavares-ui <jorleytavares@gmail.com>
2026-09-06 18:55:15 -0300

    fix: instala worklets apenas para resolucao do babel (nativewind exige)

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind", reanimated: false, worklets: false }],
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
     plugins: ["react-native-reanimated/plugin"],
